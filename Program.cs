9310898b8d7571ea474ac2c245c61b0c59115988
d13gu1n <d.acevedom1993@gmail.com>
2025-07-14 21:04:21 -0400

    Added new Console.Write() lines to app

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿Console.WriteLine("Hello, World!");
+// Console.Write("Congratulations!");
+// Console.Write(" ");
+// Console.Write("You wrote your first lines of code.");
+Console.WriteLine("Congratulations");
+Console.Write("Your wrote your first lines of code.");
+Console.WriteLine("");
 Console.WriteLine("bello na");
 Console.WriteLine('b');
 Console.WriteLine(123);
