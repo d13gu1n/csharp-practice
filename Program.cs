54b61a30ccf910057448127f308f2147e467e8dc
d13gu1n <d.acevedom1993@gmail.com>
2025-07-15 22:38:46 -0400

    Added escape characters and Japanese console writing

--- Program.cs
+++ Program.cs
@@ -42,3 +42,33 @@ firstName = "Yasmin";
 Console.WriteLine(firstName);
 
 var message = "Hello World!";
+
+string name = "Bob";
+int chalNumber = 3;
+float chalDecimal = 34.4f;
+Console.Write("Hello, " + name + "!");
+Console.Write(" ");
+Console.Write("You have " + chalNumber + " messages in your inbox.");
+Console.Write(" ");
+Console.Write("The temperature is " + chalDecimal + " celsius.");
+
+// Activity 3
+
+Console.WriteLine("Hello\nWorld!");
+Console.WriteLine("Hello\tWorld!");
+Console.WriteLine("Hello \"World\"!");
+Console.WriteLine("c:\\source\\repos");
+
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\"... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+
+Console.WriteLine(@"    c:\source\repos
+        (this is where your code goes)");
+
+Console.WriteLine(@"c:\invoices");
+Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World");
+
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+Console.WriteLine(@"c:\invoices\app.exe -j");
